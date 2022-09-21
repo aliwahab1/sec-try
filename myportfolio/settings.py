aa099fe8340befd8eadbd0f746363987d2ae2156
aliwahab <youssufsany@gmail.com>
2022-09-21 21:29:00 +0100

    fourth commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.joi(BASE_DIR, 'staticfiles')
-STATICFIILES_DIRS = (os.path.joi(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFIILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
 
